d9c2728e1cdfd56539d8f84828d509980c362d44
Omar Azzat <oazzat19@gmail.com>
2018-09-06 07:14:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function theBeatlesPlay(musicians,instruments){
   var array = []
-  
-  for(var i=0;i<musicians;i++){
+  var i = 0
+  for(i=0;i<musicians;i++){
     array.push(`${musicians[i]} plays ${instruments[i]}`)
   }
   return array
