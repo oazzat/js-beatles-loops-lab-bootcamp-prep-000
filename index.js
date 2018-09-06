1018345c6849b7ca33856a4b0d0879c7f7e70bb5
Omar Azzat <oazzat19@gmail.com>
2018-09-06 07:19:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,3 +6,12 @@ function theBeatlesPlay(musicians,instruments){
   }
   return array
 }
+
+function johnLennonFacts(facts){
+  var i = 0
+  while (i < facts.length){
+    facts[i] = facts[i] + "!!!"
+    i++
+  }
+  return facts
+}
